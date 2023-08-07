0762d7ae214998bb1b6b9d7262d837072a58fc0b
Nikola-Nino <74680506+Nikola-Nino@users.noreply.github.com>
2023-08-08 00:34:31 +0200

    Adding styling for Todo component and rendering conditional style

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import classes from "./Todo.module.css";
 import { RiCloseCircleLine } from "react-icons/ri";
 import { TiEdit } from "react-icons/ti";
 import TodoForm from "./TodoForm";
@@ -22,13 +23,25 @@ const Todo = (props) => {
   }
 
   return props.todos.map((todo, index) => (
-    <div key={todo.id}>
-      <div>{todo.text}</div>
-      <div>
-        <RiCloseCircleLine onClick={() => props.removeTodo(todo.id)} />
-        <TiEdit onClick={() => setEdit({ id: todo.id, value: todo.text })}/>
+    <>
+      <div
+        className={
+          todo.isComplete
+            ? `${classes} todo-row complete`
+            : `${classes} todo-row`
+        }
+        key={index}
+      ></div>
+      <div key={todo.id}>
+        <div key={todo.id} onClick={() => props.completeTodo(todo.id)}>
+          {todo.text}
+        </div>{" "}
+        <div className={classes.icons}>
+          <RiCloseCircleLine className={classes["delete-icon"]} onClick={() => props.removeTodo(todo.id)} />
+          <TiEdit className={classes["edit-icon"]} onClick={() => setEdit({ id: todo.id, value: todo.text })} />
+        </div>
       </div>
-    </div>
+    </>
   ));
 };
 
